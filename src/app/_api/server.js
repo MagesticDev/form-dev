b5baf98755cd9d6bebaf67a2a3fea5eee686f3b0
Friday <romain.bardey@free.fr>
2022-06-27 15:29:58 +0200

    dirty Express for auth

---
+++ src/app/_api/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const jsonwebtoken = require("jsonwebtoken");
+
+const JWT_SECRET = "vWYXBK$Hy$A86PWjRC52NgX15Ag&PP%vfgWgS@8TcSmmJthdb*dD6v!XVzd5";
+const admin = {
+  email: 'admin@form-dev.fr',
+  name: 'admin',
+  roles: ['ROLE_USER','ROLE_ADMIN']
+}
+
+const app = express();
+
+app.use(express.json());
+app.post("/login",(req,res)=> {
+  const {email, password} = req.body;
+  if(email === 'admin@form-dev.fr' && password === 'admin') { // HARD CODED ADMIN USER
+    return res.json({
+      token: jsonwebtoken.sign({user: 'admin@form-dev.fr'}, JWT_SECRET),
+      user: {
+        ...admin
+      }
+    })
+  }
+  return res.status(401).json({message: "Invalid username and password."});
+});
+
+app.listen(8080, () => {
+  console.log("API running on localhost:8080")
+});
+
+app.listen()
