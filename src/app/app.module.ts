c89e487ca764ace2f61f2ae1eda633a9f94f5607
Friday <romain.bardey@free.fr>
2022-06-27 15:05:05 +0200

    add interceptor + import httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { AppFooterComponent } from './components/main/app-footer/app-footer.component';
 import { AppHeaderComponent } from './components/main/app-header/app-header.component';
 import { SharedsModule } from './shareds';
+import {AuthInterceptor} from "@/core/helpers/auth.interceptor";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -14,10 +17,18 @@ import { SharedsModule } from './shareds';
   ],
   imports: [
     BrowserModule,
-    SharedsModule
+    HttpClientModule,
+    SharedsModule,
+    AppRoutingModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
