4de9ab4dadd849c853fab358b8ec56c244004aa8
Friday <romain.bardey@free.fr>
2022-06-28 15:09:39 +0200

    PrimeNG imports + default styling on app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,37 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
+import {InputTextModule} from 'primeng/inputtext';
+
 import { AppComponent } from './app.component';
 import { AppFooterComponent } from './components/main/app-footer/app-footer.component';
 import { AppHeaderComponent } from './components/main/app-header/app-header.component';
 import { SharedsModule } from './shareds';
 import {AuthInterceptor} from "@/core/helpers/auth.interceptor";
 import {AppRoutingModule} from "./app-routing.module";
+import {LoginComponent} from "./components/pages/login/login.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
 
 @NgModule({
   declarations: [
     AppComponent,
     AppFooterComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+
+    InputTextModule,
+    ButtonModule,
+    RippleModule,
+
     SharedsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
